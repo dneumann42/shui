9216b2ec09f1a321a9c05bfcdf7e8654cf4a8888
dneumann42 <dneumann42@proton.me>
2026-02-01 15:10:50 -0800

    clean

--- tests/test_label.nim
+++
@@ -1,8 +0,0 @@
-import ../src/shui
-
-# Test that label() function was generated from component
-let myLabel = label("Hello World")
-
-echo "Label created!"
-echo "Label state: ", myLabel.state
-echo "Label title: ", myLabel.state.title
